c7c4f83a3f1c76f960ce87ff9bb86a438cf81f6e
Justin Jenkins <justinjenkins@gmail.com>
2019-06-12 16:32:59 -0700

    Update index.blade.php
    Add examples

--- app/resources/views/index.blade.php
+++ app/resources/views/index.blade.php
@@ -20,14 +20,42 @@
                 <h1 class="title">
                     Hello <i class="fas fa-church"></i>
                 </h1>
-                <h2 class="subtitle has-text-white-ter">Give VerseSee a try!</h2>
+                <h2 class="subtitle has-text-white-ter">Nice to see ya, welcome to VerseSee.</h2>
                 <div class="buttons columns is-mobile is-centered">
-                    <a class="button is-large is-white" href="https://twitter.com/intent/tweet?text=@versesee" target="_blank"><i class="fab fa-twitter"></i>&nbsp;@versesee</a>
-                    <a class="button is-large is-white" href="sms://+17194284282"><i class="fas fa-mobile-alt"></i>&nbsp;versesee</a>
+                    <a class="button is-white" href="https://twitter.com/intent/tweet?text=@versesee" target="_blank"><i class="fab fa-twitter"></i>&nbsp;@versesee</a>
+                    <a class="button is-white" href="sms://+17194284282"><i class="fas fa-mobile-alt"></i>&nbsp;<small>(719)-428-4282</small></a>
                 </div>
-                <p>&nbsp;</p>
+                <h4 class="title is-4">How do I use this?</h4>
+
+                <div class="container" >
+                    <div class="columns is-centered is-vcentered ">
+                        <div class="column is-narrow has-text-centered">
+
+                <article class="message is-info is-small is-pulled-left" style="width: 310px; margin-right: 10px;">
+                    <div class="message-body">
+                        Text the word <strong>pray</strong> to <a style="text-decoration: none;" href="sms://+17194284282">(719)-428-4282</a>
+                    </div>
+                </article>
+                <article class="message is-info is-small is-pulled-left" style="width: 310px; margin-right: 10px;">
+                    <div class="message-body">
+                        Tweet the word <strong>care</strong> to <strong>@versesee</strong>
+                    </div>
+                </article>
+                <article class="message is-info is-small is-pulled-left" style="width: 310px;">
+                    <div class="message-body">
+                        Text the words <strong>good fruit</strong> to <a style="text-decoration: none;"  href="sms://+17194284282">(719)-428-4282</a>
+                    </div>
+                </article>
+
+                        </div>
+                    </div>
+                </div>
+
+                <p class="is-clearfix">&nbsp</p>
+                <p class="is-clearfix">&nbsp</p>
+
                 <h6 class="title is-5">
-                    I bet you are interested in the <a class="button is-small is-dark" href="https://github.com/justinjenkins/churchdebugger.com" target="_blank"><span class="icon is-small"><i class="fab fa-github"></i></span><span>source</span></a>
+                    Go ahead, check out the <a class="button is-small is-dark" href="https://github.com/justinjenkins/churchdebugger.com" target="_blank"><span class="icon is-small"><i class="fab fa-github"></i></span><span>source</span></a>
                 </h6>
 
             </div>
