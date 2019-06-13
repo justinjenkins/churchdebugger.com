f1bd6aef41b13d9115c1fd1cecd7f635a1dcae85
Justin Jenkins <justinjenkins@gmail.com>
2019-06-12 17:33:05 -0700

    Twiml class

---
+++ app/app/Common/Twiml.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Common;
+
+use Twilio\Twiml as TTwiml;
+
+class Twiml {
+
+    protected $twiml;
+
+    public function __construct() {
+        $this->twiml = new TTwiml;
+    }
+
+    public function respond_with_text(string $response_text, array $params) {
+
+        $defaults = array (
+            "text_sent" => null,
+            "media" => null,
+        );
+
+        $params = $params + $defaults;
+
+        $message = $this->twiml->message();
+
+        $message->body(htmlentities($response_text));
+
+        if ($params["media"]) {
+            $message->media($params["media"]);
+        }
+
+        return $this->twiml;
+    }
+
+}
