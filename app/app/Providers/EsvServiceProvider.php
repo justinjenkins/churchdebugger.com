2b7ddf50697744bbfff3e89b54f3da92ad12a6b4
Justin Jenkins <justinjenkins@gmail.com>
2019-06-12 17:55:40 -0700

    Adding ESV provider (not currently using)

---
+++ app/app/Providers/EsvServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class EsvServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->client = new GuzzleHttp\Client(['headers' => ["Authorization" => "Token ".env("ESV_TOKEN")]]);
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
