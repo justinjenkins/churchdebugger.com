5956e5f0a346a38fdbb5a275f3bcf8d4911f3383
Justin Jenkins <justinjenkins@gmail.com>
2019-06-13 20:53:25 -0700

    Update Models and fillables

--- app/app/Text.php
+++ app/app/Text.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Text extends Model
 {
-    //
+    protected $fillable = [
+        'text_sent',
+    ];
 }
--- app/app/TweetHistory.php
+++ app/app/TweetHistory.php
@@ -8,6 +8,14 @@ class TweetHistory extends Model {
 
     protected $table = 'tweet_history';
 
+    protected $fillable = [
+        'twitter_id',
+        'username',
+        'tweet',
+        'tweet_created_at',
+        'raw_tweet'
+    ];
+
     public function scopeLatestFirst($query) {
         return $query->orderBy('twitter_id', 'desc');
     }
