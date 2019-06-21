bf6366c5fa6cb84ad94aa38e5a234288a910b876
Justin Jenkins <justinjenkins@gmail.com>
2019-06-21 00:06:34 -0700

    Update TwitterMentions to add fillable and scopes

--- app/app/TwitterMentions.php
+++ app/app/TwitterMentions.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class TwitterMentions extends Model
 {
-    //
+    protected $fillable = [
+        'twitter_id',
+        'username',
+        'tweet',
+        'tweet_created_at',
+        'raw_tweet',
+        'processed_at'
+    ];
+
+    public function scopeLatestFirst($query) {
+        return $query->orderBy('twitter_id', 'desc');
+    }
+
+    public function scopeLastProcessed($query) {
+        return $query->whereNotNull('processed_at')->orderBy('twitter_id', 'desc')->first();
+    }
+
+    public function scopeLatestUnprocessedFirst($query) {
+        return $query->whereNull('processed_at')->orderBy('twitter_id', 'desc');
+    }
 }
