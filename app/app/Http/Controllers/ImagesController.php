d73f3f27694e078a11c624fc408e8d1c59ab052e
Justin Jenkins <justinjenkins@gmail.com>
2019-06-21 01:37:35 -0700

    Remove unused boilerplate code, refactor names

--- app/app/Http/Controllers/ImagesController.php
+++ app/app/Http/Controllers/ImagesController.php
@@ -8,82 +8,46 @@ use Illuminate\Http\Request;
 
 class ImagesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
-    }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Image  $image
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Image $image, Request $request)
-    {
-        $term = $request->query('term');
+        $message = $request->query('message');
         $tid = $request->query('tid');
-        return Images::generate($term, $tid);
+
+        $image = new Image;
+        $image->imageid = Image::generate_imageid();
+        $image->message = $message;
+        $image->twitter_id = $tid;
+        $image->save();
+
+        Images::generate($message, $image->imageid, false);
+
+        return redirect("/images/".$image->imageid);
+
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Image  $image
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Image $image)
+    public function show(Image $image)
     {
-        //
+        return view('images.show', compact('image'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Image  $image
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Image $image)
+    public function download(Image $image, Request $request)
     {
-        //
+
+        $message = "";
+        $imageid = null;
+
+        if (!$image->exists) {
+            $message = $request->query('message');
+        } else {
+            $message = $image->message;
+            $imageid = $image->imageid;
+        }
+
+        $response = Images::generate($message, $imageid);
+
+        return response($response)->header('Content-Type', 'image/jpg');
+
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Image  $image
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Image $image)
-    {
-        //
-    }
 }
