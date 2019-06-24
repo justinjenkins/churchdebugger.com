15c0468a1031cbb845cb05cf8805eaf4186b125d
Justin Jenkins <justinjenkins@gmail.com>
2019-06-24 01:56:57 -0700

    Fix name

--- app/tests/Unit/Common/UnsplashTest.php
+++ app/tests/Unit/Common/UnsplashTest.php
@@ -8,11 +8,11 @@ use App\Common\Unsplash;
 class UnsplashTest extends TestCase
 {
 
-    public function test_fix_image_url_dimentions() {
+    public function test_fix_image_url_dimensions() {
 
         $url = "https://images.unsplash.com/photo-1523311964370-42ac336c878b?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb";
 
-        $updated_url = Unsplash::fix_image_url_dimentions($url, ["fit" => "crop", "width" => 1080, "height" => 720]);
+        $updated_url = Unsplash::fix_image_url_dimensions($url, ["fit" => "crop", "width" => 1080, "height" => 720]);
         $this->assertEquals(
             "https://images.unsplash.com/photo-1523311964370-42ac336c878b?fit=crop&w=1080&h=720&ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb",
             $updated_url
