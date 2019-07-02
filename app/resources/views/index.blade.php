90f12526da89490f07268260ce832570d7af839e
Justin Jenkins <justinjenkins@gmail.com>
2019-07-01 18:19:24 -0700

    Improving homepage layout

--- app/resources/views/index.blade.php
+++ app/resources/views/index.blade.php
@@ -35,47 +35,15 @@
                 <div class="column"></div>
 
                 <h5 class="title is-5">
-                    <a class="button is-large" href="/timeline"><span class="icon"><i class="fas fa-book-open"></i></span> <span>timeline (beta)</span></a>
+                    <a class="button is-large" href="/timeline"><span class="icon"><i class="fas fa-book-open"></i></span> <span>timeline</span></a>
+                    <a class="button is-large" href="/seek"><span class="icon"><i class="fas fa-search"></i></span> <span>seek</span></a>
                 </h5>
 
                 <div class="column"></div>
 
-                <h4 class="title is-4">How do I use this?</h4>
-
-                <div class="container">
-                    <div class="columns is-centered is-vcentered">
-                        <div class="column is-narrow has-text-centered" style="margin: auto;">
-
-                        <article class="message is-info is-small is-pulled-left" style="width: 310px; margin-right: 10px;">
-                            <div class="message-body">
-                                Text the word <strong>pray</strong> to <a style="text-decoration: none;" href="sms://+17194284282">(719)-428-4282</a>
-                            </div>
-                        </article>
-                        <article class="message is-info is-small is-pulled-left" style="width: 310px; margin-right: 10px;">
-                            <div class="message-body">
-                                Tweet the word <strong>care</strong> to <strong>@versesee</strong>
-                            </div>
-                        </article>
-
-                        </div>
-                    </div>
-                </div>
-
-                <div class="column">&nbsp;</div>
-
-                <div class="buttons columns is-mobile is-centered">
-                    <a class="button is-white" href="https://twitter.com/intent/tweet?text=@versesee" target="_blank"><i class="fab fa-twitter"></i>&nbsp;@versesee</a>
-                    <a class="button is-white" href="sms://+17194284282"><i class="fas fa-mobile-alt"></i>&nbsp;<small>(719)-428-4282</small></a>
-                </div>
-
-
                 <h5 class="title is-5">
-                    Go ahead, check out the <a class="button is-small is-dark" href="https://github.com/justinjenkins/churchdebugger.com" target="_blank"><span class="icon is-small"><i class="fab fa-github"></i></span><span>source</span></a>
+                    Go ahead, check out the <a class="button is-small is-black" href="https://github.com/justinjenkins/churchdebugger.com" target="_blank"><span class="icon is-small"><i class="fab fa-github"></i></span><span>source</span></a>
                 </h5>
-
-                <h6 class="title is-6"><span class="tag">VerseSee does not track your phone number</span></h6>
-
-                <h5 class="title is-5">Examples</h5>
 
                 <div class="tile is-ancestor">
                     <div class="tile is-parent">
@@ -110,6 +78,37 @@
                         </article>
                     </div>
                 </div>
+
+
+                <h4 class="title is-4">How else can I use this?</h4>
+
+                <div class="container">
+                    <div class="columns is-centered is-vcentered">
+                        <div class="column is-narrow has-text-centered" style="margin: auto;">
+
+                            <article class="message is-info is-small is-pulled-left" style="width: 310px; margin-right: 10px;">
+                                <div class="message-body">
+                                    Text the word <strong>pray</strong> to <a style="text-decoration: none;" href="sms://+17194284282">(719)-428-4282</a>
+                                </div>
+                            </article>
+                            <article class="message is-info is-small is-pulled-left" style="width: 310px; margin-right: 10px;">
+                                <div class="message-body">
+                                    Tweet the word <strong>care</strong> to <strong>@versesee</strong>
+                                </div>
+                            </article>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="column">&nbsp;</div>
+
+                <div class="buttons columns is-mobile is-centered">
+                    <a class="button is-white" href="https://twitter.com/intent/tweet?text=@versesee" target="_blank"><i class="fab fa-twitter"></i>&nbsp;@versesee</a>
+                    <a class="button is-white" href="sms://+17194284282"><i class="fas fa-mobile-alt"></i>&nbsp;<small>(719)-428-4282</small></a>
+                </div>
+
+                <h6 class="title is-6"><span class="tag is-light">VerseSee does not track your phone number</span></h6>
 
             </div>
 
